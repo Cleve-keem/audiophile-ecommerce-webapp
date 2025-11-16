aa3989e28061f69c729e80ef23067f02e830a3ee
Cleve-keem <belloharyourmidey@gmail.com>
2025-11-16 09:25:51 -0600

    adj: changed footer text color to grey

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -8,20 +8,20 @@ export default function Footer() {
         <div className="flex flex-col items-center text-center">
           <Logo className="my-5" size="25" />
           <ul className="flex flex-col gap-4 uppercase my-5">
-            <li>
+            <li className="text-[13px]">
               <Link href="">Home</Link>
             </li>
-            <li>
+            <li className="text-[13px]">
               <Link href="">Headphones</Link>
             </li>
-            <li>
+            <li className="text-[13px]">
               <Link href="">speakers</Link>
             </li>
-            <li>
+            <li className="text-[13px]">
               <Link href="">earphones</Link>
             </li>
           </ul>
-          <p className="mt-4 mb-5 leading-text text-[15px]">
+          <p className="mt-4 mb-5 leading-text text-[15px] px-5 text-primary-grey">
             Audiophile is an all in one stop to fulfill your audio needs. We're
             a small team of music lovers and sound specialists who are devoted
             to helping you get the most out of personal audio. Come and visit
@@ -29,7 +29,9 @@ export default function Footer() {
           </p>
 
           <div>
-            <p className="text-[15px] leading-text">Copyright 2021. All Rights Reserved</p>
+            <p className="text-[15px] leading-text text-primary-grey">
+              Copyright 2021. All Rights Reserved
+            </p>
             <ul className="flex">
               <Link href=""></Link>
               <Link href=""></Link>
