924f9be474d5bab90cedeb560b1df2b9de995e89
Cleve-keem <belloharyourmidey@gmail.com>
2025-11-17 19:45:52 -0600

    fix: fixed page title not showing and removed accessories component

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -2,8 +2,8 @@
 
 import { usePathname } from "next/navigation";
 
-export default function PageTitle({ title }: { title: string }) {
-  // const title = usePathname()?.split("/").pop();
+export default function PageTitle() {
+  const title = usePathname()?.split("/").pop();
   return (
     <div className="flex justify-center py-5 bg-foreground">
       <h1 className="text-h4 uppercase text-secondary-white tracking-h1">
