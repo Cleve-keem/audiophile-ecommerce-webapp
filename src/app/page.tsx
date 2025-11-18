83aede9ee4c8e24420639b6ee50ffb3003eebc16
Cleve-keem <belloharyourmidey@gmail.com>
2025-11-17 19:36:59 -0600

    refactored my home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Accessories from "@/components/Accessories";
 import AudioGear from "@/components/AudioGear";
 import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
+import ProductList from "@/components/Products";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Accessories />
+      <Menu />
+      <ProductList />
       <AudioGear />
     </>
   );
